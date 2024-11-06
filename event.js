63ef16439b3f5a8a11ab41ab3b56217fb8f9181f
rakshak kalla <rakshak1775@gmail.com>
2024-11-07 02:44:15 +0530

    events are added

---
+++ event.js
@@ -0,0 +1,47 @@
+const card1=document.querySelector("div")
+card1.id="physics"
+card1.innerHTML='<img src="https://thumbs.dreamstime.com/b/woman-face-recognition-biometric-verification-woman-face-recognition-biometric-verification-concept-107714380.jpg"width="320" /><p>this is physics </P> <button> Explore </button>'
+card1.style.border="2px solid black"
+card1.style.display="inline-block"
+card1.style.backgroundColor="lightpink"
+card1.style.padding="20px"
+card1.style.textAlign="center"
+card1.style.borderRadius="30px"
+
+
+const card2=document.getElementById("car")
+card2.innerHTML='<img src="https://packardinfo.com/xoops/html/uploads/newbb/462_4c47afb0f05a3.jpg" width="320"/><p>this is vintage car </P> <button> Explore </button>'
+card2.style.border="2px solid black"
+card2.style.display="inline-block"
+card2.style.backgroundColor="lightgreen"
+card2.style.padding="20px"
+card2.style.textAlign="center"
+card2.style.borderRadius="30px"
+
+
+
+const card3=document.createElement("div")
+card3.id="bike"
+card3.innerHTML='<img src="https://www.bikebound.com/wp-content/uploads/2022/04/Handbuilt-333-1080333-800x533.jpg" width="320"/><p>this is royal bike </P> <button> Explore </button>'
+card3.style.border="2px solid black"
+card3.style.display="inline-block"
+card3.style.backgroundColor="lightgreen"
+card3.style.padding="20px"
+card3.style.textAlign="center"
+card3.style.borderRadius="30px"
+document.body.appendChild(card3)
+
+
+const card4=document.createElement("div")
+card4.id="bird"
+card4.innerHTML='<img src="https://media-01.imu.nl/storage/kijkenziefotoschool.nl/10365/bramoostdijk-800x532.jpg" width="320"/><p>this is king fisher </P> <button> Explore </button>'
+card4.style.border="2px solid black"
+card4.style.display="inline-block"
+card4.style.backgroundColor="lightgreen"
+card4.style.padding="20px"
+card4.style.textAlign="center"
+card4.style.borderRadius="30px"
+card4.style.marginLeft="4px"
+document.body.appendChild(card4)
+
+
